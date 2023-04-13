c4a18d256156c21f9224f5cfb57ccb64bcdd52ff
dengsong666 <dengsong2022@gmail.com>
2023-04-13 08:34:58 +0800

    feat

--- transaction_data/collect.py
+++ transaction_data/collect.py
@@ -6,5 +6,6 @@ from datetime import timedelta, datetime
 # 获取中证指数截至前一天每日数据
 index = requests.get("https://www.csindex.com.cn/csindex-home/perf/index-perf",
                         params={'indexCode': '931151', 'startDate': '19900101',
-                                'endDate': (datetime.today() + timedelta(-1)).strftime('%Y%m%d')}).json().get('data')
+                                'endDate': (datetime.today() + timedelta(-1)).strftime('%Y%m%d')})
+df = pd.DataFrame(index.json().get('data')).drop(columns=['indexNameCnAll', 'indexNameCn','indexNameEnAll','indexNameEn'], errors='ignore')
 print(index)
